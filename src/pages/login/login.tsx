573af70a0ad3acaa3dafd22af447552b714882fe
Rafael Lacerda <rafa_jnd@hotmail.com>
2025-01-31 10:38:54 -0300

    feat: Criacao do layout da tela de login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,3 +1,79 @@
+import logoBanco from '../../assets/logo-white.png';
+
+import { Input } from '@/components/input/input';
+import { Button } from '@/components/button/button';
+import { ButtonLink } from '@/components/buttonLink/buttonLink';
+import { Checkbox } from '@/components/checkbox/checkbox';
+
+import {
+	BackgroundWoman,
+	Overlay,
+	Logo,
+	Main,
+	ContainerForm,
+	Wrapper,
+	TitleGroup,
+	Title,
+	Subtitle,
+	GroupNewAccount,
+	GroupButtonUser,
+} from './styles';
+
 export default function Login() {
-	return <h1>Login</h1>;
+	const handleSubmit = () => {};
+
+	return (
+		<Main>
+			<BackgroundWoman>
+				<Logo src={logoBanco} />
+				<Overlay />
+			</BackgroundWoman>
+			<ContainerForm>
+				<Wrapper>
+					<TitleGroup>
+						<Title>
+							Bem vindo <span>:)</span>
+						</Title>
+						<Subtitle>Entre na sua conta</Subtitle>
+					</TitleGroup>
+
+					<form onSubmit={handleSubmit} className="space-y-6" noValidate>
+						<div>
+							<Input
+								label="Usuário"
+								id="username"
+								type="text"
+								// value={username}
+								// onChange={e => setUsername(e.target.value)}
+								required
+							/>
+						</div>
+
+						<div>
+							<Input
+								label="Senha"
+								id="password"
+								type="password"
+								// value={password}
+								// onChange={e => setPassword(e.target.value)}
+								required
+							/>
+						</div>
+
+						<GroupButtonUser>
+							<Checkbox id="remember" label="Lembrar dispositivo" />
+							<ButtonLink variant="link">Esqueceu sua senha?</ButtonLink>
+						</GroupButtonUser>
+
+						<Button type="submit">ENTRAR</Button>
+					</form>
+
+					<GroupNewAccount>
+						<span>Não tem conta? </span>
+						<ButtonLink size="fs-2">Faça uma agora</ButtonLink>
+					</GroupNewAccount>
+				</Wrapper>
+			</ContainerForm>
+		</Main>
+	);
 }
