6b37baa084df7e8215e628ac38797b1d8ba499c6
Rafael Lacerda <rafa_jnd@hotmail.com>
2025-01-31 10:37:56 -0300

    chore: Mensagem no error boundary

--- src/components/errorBoundary/errorBoundary.tsx
+++ src/components/errorBoundary/errorBoundary.tsx
@@ -21,7 +21,7 @@ class ErrorBoundary extends Component<Props, State> {
 
 	render() {
 		if (this.state.hasError) {
-			return <h1>Something went wrong.</h1>;
+			return <h1>Algo deu errado!</h1>;
 		}
 
 		return this.props.children;
